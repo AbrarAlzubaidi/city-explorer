b14c782703de9f4563e2a5a218ee4555eb308626
AbrarAlzubaidi <https://github.com/AbrarAlzubaidi>
2021-09-12 23:03:35 +0300

    solve card 3

--- src/component/LocationView.js
+++ src/component/LocationView.js
@@ -6,7 +6,7 @@ class LocationView extends Component {
                 <h2>City name: {this.props.cityName}</h2>
                 <h4> lon: {this.props.lon}</h4>
                 <h4> lat: {this.props.lat}</h4>
-                <img src={this.props.map} alt='..'/>
+                <img src={this.props.map} alt='.....'/>
 
             </div>
         )
