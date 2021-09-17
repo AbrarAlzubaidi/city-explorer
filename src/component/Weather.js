0b9f90b74a1cadc6e29373aaeee9fa69108beff4
AbrarAlzubaidi <https://github.com/AbrarAlzubaidi>
2021-09-18 01:09:17 +0300

    fixed

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react'
-import {Alert} from 'react-bootstrap';
+import { Alert, Col } from 'react-bootstrap';
 
 class Weather extends Component {
     render() {
-        let cityName= this.props.cityName;
-        let date= this.props.date;
-        let description= this.props.description
+        let cityName = this.props.cityName;
+        let date = this.props.date;
+        let description = this.props.description
 
-        console.log('date',date);
-        
-        
         return (
+            <Col>
+                <Alert variant="success">
+                    <Alert.Heading>{cityName}</Alert.Heading>
+                    <p>
+                        the date: {date}
+                    </p>
+                    <hr />
+                    <p className="mb-0">
+                        the weather of the date: {description}
+                    </p>
+                </Alert></Col>
 
-            <Alert variant="success">
-                <Alert.Heading>{cityName}</Alert.Heading>
-                <p>
-                    the date: {date}
-                </p>
-                <hr />
-                <p className="mb-0">
-                    the weather of the date: {description}
-                </p>
-            </Alert>
         )
     }
 }
